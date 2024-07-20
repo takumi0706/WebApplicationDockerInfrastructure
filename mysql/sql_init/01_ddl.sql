c132e8f48a3ccef5302388977e7bd21b8e9370de
takumi <ganndamu0706@gmail.com>
2024-07-20 19:14:56 +0900

    Feat:なんかいろいろ微調整しましたとさ

--- mysql/sql_init/01_ddl.sql
+++ mysql/sql_init/01_ddl.sql
@@ -1,10 +1,10 @@
 -- データベースの作成
-CREATE DATABASE IF NOT EXISTS spring_dev CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS wadidb CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- データベースの使用
-USE spring_dev;
+USE wadidb;
 
 -- テーブルの作成
-CREATE TABLE IF NOT EXISTS spring_dev.user_info
+CREATE TABLE IF NOT EXISTS wadidb.user_info
 (
     login_id VARCHAR(50) NOT NULL PRIMARY KEY,
     password_hash VARCHAR(255) NOT NULL
